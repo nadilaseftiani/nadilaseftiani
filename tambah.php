1bd8fdb72c8b4bfca24e73f43af6939c1f4c0d49
nadilaseftiani <160471081+nadilaseftiani@users.noreply.github.com>
2024-02-19 15:52:49 +0700

    Galley

---
+++ tambah.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HALAMAN ADMIN</h1>
+    <a href="tambah.php">tambah data</a>
+    <table border=1 >
+        <thead>
+        <tr>
+                <th>No</th>
+                <th>Foto kegiatan</th>
+                <th>judul kegiatan</th>
+                <th>tanggal</th>
+                <th>action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while($row = mysqli_fetch_assoc($hasil)): ?>
+            <tr>
+                <td>1</td>
+                <td><img src="gambar/<?= $row["foto"]?>" width="100px" alt="")</td>
+                <td><?= $row["keterangan"]?></td>
+                <td><?= $row["tanggal"]?></td>
+                <td>
+                    <a href="">edit</a> | <a href="">hapus</a>
+                </td>
+            </tr>
+            <?php endwhile ; ?>
+
+        </tbody>
+        
+        
+        </thead>
+
+    </table>
+
+
+</body>
+</html>
